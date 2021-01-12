2050003e1c6e9a19a0980804a939ec0809b69453
kevin <kevinalvaro.cunyasquispe@gmail.com>
2021-01-12 19:36:07 +0100

    despegando

---
+++ src/main/java/prueba/Iniciar.java
@@ -0,0 +1,13 @@
+package prueba;
+
+public class Iniciar {
+
+	public static void main(String[] args) {
+		System.out.println("hola");
+		
+		Persona person = new Persona("kevin");
+		
+		person.imprimir();
+	}
+
+}
---
+++ src/main/java/prueba/Persona.java
@@ -0,0 +1,18 @@
+package prueba;
+
+public class Persona {
+
+	
+	private String nombre;
+	
+	public Persona(String nombre) {
+		
+		this.nombre = nombre;
+		
+	}
+	
+	public void imprimir() {
+		System.out.println(this.nombre);
+	}
+	
+}
